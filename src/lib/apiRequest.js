d7f5f1ce0be7d9e040486711b12513eaa01a6f0f
Berlino126 <126323198+Berlino126@users.noreply.github.com>
2025-03-20 17:19:04 +0100

    Delete src directory

--- src/lib/apiRequest.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const apiRequest = axios.create({
-  //baseURL: "http://35.181.57.245:443/api",
-  baseURL: "http://localhost:8800/api",
-  withCredentials: true,
-});
-
-export default apiRequest;
